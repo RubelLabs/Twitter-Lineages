3bb99a15001e1a56b2df5c5caed96785c8a4ccfc
Sadit Rubel <saditrubel09@gmail.com>
2025-10-02 21:58:13 +0100

    Initial Commit

---
+++ project.py
@@ -0,0 +1,25 @@
+# As of the September 11th, 2025 I have thought of my project title.
+# It is called "Twitter Lineages"
+# It uses graph theory to find the shortest path between two people in a social network.
+# Essentially you enter your Twitter handle and the program will find the shortest path between you and a bunch of famous people in the world.
+# It's based on the Six Degrees of Kevin Bacon.
+
+# It will cover the 5 celebrities Elon Musk, Donald Trump, ... T.B.D.
+
+def main():
+    ...
+    
+def function_1():
+    ...
+
+
+def function_2():
+    ...
+
+
+def function_n():
+    ...
+
+if __name__ == "__main__":
+    main()
+
